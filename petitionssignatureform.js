6916d4f54c8fdab2cad231f59747b0510373118d
TravisCarden <Travis_R_Carden@oa.eop.gov>
2014-04-02 09:29:16 -0500

    PT-1308 - Made improvements per code review.

--- petitionssignatureform.js
+++ petitionssignatureform.js
@@ -6,15 +6,17 @@
    */
   Drupal.behaviors.petitionssignatureformFormValidation = {
     attach: function () {
-      $('#petitionssignatureform-signature-form').submit(function () {
+      $('#petitionssignatureform-signature-form').submit(function (event) {
         var emailField = $('#edit-email'), emailValue = emailField.val().trim(), pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         if (!pattern.test(emailValue)) {
           emailField.next('.error-message').remove();
           emailField.addClass('error');
-          return false;
+          event.preventDefault();
         }
-        emailField
-          .removeClass('error');
+        else {
+          emailField
+            .removeClass('error');
+        }
       });
     }
   };
