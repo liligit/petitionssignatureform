ffa9a325d361c54208b4087791dcad08630a7280
TravisCarden <Travis_R_Carden@oa.eop.gov>
2014-04-03 11:57:22 -0500

    PT-1304 - Made signature form constructor accept a petition ID as an argument.

---
+++ lib/PetitionssignatureformException.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Contains PetitionssignatureformException.
+ */
+
+/**
+ * An exception class for the Petitions Signature Form module.
+ */
+class PetitionssignatureformException extends Exception {}
---
+++ tests/Tests/PetitionssignatureformModuleTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Contains PetitionssignatureformModuleTest.
+ */
+
+require_once __DIR__ . '/../../petitionssignatureform.module';
+
+/**
+ * Tests the functions in petitionssignatureform.module.
+ *
+ * @todo Test that the signature form constructor hides the Petition ID field
+ *   and supplies a default value if a petition ID is supplied.
+ * @todo Test that signature form constructor shows the Petition ID field and
+ *   does not supply a default value if a petition ID is *not* supplied.
+ */
+class PetitionssignatureformModuleTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests that the signature form constructor rejects an invalid petition ID.
+   *
+   * @dataProvider providerSignatureFormRejectsInvalidPetitionId
+   *
+   * @expectedException PetitionssignatureformException
+   *
+   * @param mixed $petition_id
+   *   An invalid petition ID value.
+   *
+   * @see petitionssignatureform_signature_form()
+   */
+  public function testSignatureFormRejectsInvalidPetitionId($petition_id) {
+    $form_state = NULL;
+    petitionssignatureform_signature_form(NULL, $form_state, $petition_id);
+  }
+
+  /**
+   * Data provider for testSignatureFormRejectsInvalidPetitionId().
+   *
+   * @see testSignatureFormRejectsInvalidPetitionId()
+   */
+  public function providerSignatureFormRejectsInvalidPetitionId() {
+    $tests[] = array(array());
+    $tests[] = array(0);
+    $tests[] = array('');
+
+    return $tests;
+  }
+
+}
